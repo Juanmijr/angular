2bcc081a3c9d7becdb2d05b5d7bdece897a151e6
Juan Miguel Jiménez <juanmiguelrbb@gmail.com>
2020-02-04 14:24:37 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NuevoUsuarioComponent } from './components/nuevo-usuario/nuevo-usuario.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NuevoUsuarioComponent,
+    UsuarioComponent,
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
