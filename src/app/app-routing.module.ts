7fc711e3b0fbed5e7c477be0711cff3ef064c631
Juan Miguel <juanmiguelrbb@gmail.com>
2020-02-06 17:51:46 +0100

    subida casa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent} from './components/principal/principal.component'; 
 import { ComponentesComponent } from './components/componentes/componentes.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 const routes: Routes = [
 
 {path:'' , component:PrincipalComponent},
-{path:'componentes', component:ComponentesComponent}
+{path:'componentes', component:ComponentesComponent},
+{path: 'iniciarSesion', component:UsuarioComponent}
 
 ];
 
